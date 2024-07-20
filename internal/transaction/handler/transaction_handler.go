4920b9bdd8680bae40ec0a3383c8dd86e3af3482
Dzkzzz <dzdzaakk@gmail.com>
2024-07-20 19:27:21 +0700

    create transaction service

---
+++ internal/transaction/handler/transaction_handler.go
@@ -0,0 +1,68 @@
+package transaction
+
+import (
+	model "Dzaakk/synapsis/internal/transaction/models"
+	usecase "Dzaakk/synapsis/internal/transaction/usecase"
+	auth "Dzaakk/synapsis/package/auth"
+	template "Dzaakk/synapsis/package/template"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type TransactionHandler struct {
+	Usecase usecase.TransactionUseCase
+}
+
+func NewTransactionHandler(usecase usecase.TransactionUseCase) *TransactionHandler {
+	return &TransactionHandler{
+		Usecase: usecase,
+	}
+}
+
+func (handler *TransactionHandler) Route(r *gin.RouterGroup) {
+	transactionHandler := r.Group("api/v1")
+
+	transactionHandler.Use()
+	{
+		transactionHandler.POST("/transaction", auth.JWTMiddleware(), func(ctx *gin.Context) {
+			if err := handler.Checkout(ctx); err != nil {
+				ctx.JSON(http.StatusInternalServerError, template.Response(http.StatusInternalServerError, "internal server error", err.Error()))
+				return
+			}
+		})
+	}
+}
+
+func (handler *TransactionHandler) Checkout(ctx *gin.Context) error {
+	var data model.TransactionReq
+	if err := ctx.ShouldBindJSON(&data); err != nil {
+		ctx.JSON(http.StatusBadRequest, template.Response(http.StatusBadRequest, "Bad Request", "Invalid input data"))
+		return nil
+	}
+	fmt.Printf("input = %v", data)
+
+	template.AuthorizedChecker(ctx, data.CustomerId)
+	if ctx.IsAborted() {
+		return nil
+	}
+
+	receipt, err := handler.Usecase.CreateTransaction(data)
+	if err != nil {
+		var statusCode int
+		var message string
+		if err.Error() == "insufficient balance" {
+			statusCode = http.StatusBadRequest
+			message = "Insufficient Balance"
+		} else {
+			statusCode = http.StatusInternalServerError
+			message = "Internal Server Error"
+		}
+
+		ctx.JSON(statusCode, template.Response(statusCode, message, err.Error()))
+		return nil
+	}
+	ctx.JSON(http.StatusCreated, template.Response(http.StatusOK, "Success", receipt))
+	return nil
+}
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	customer "Dzaakk/synapsis/internal/customer/injector"
 	product "Dzaakk/synapsis/internal/product/injector"
 	shoppingCart "Dzaakk/synapsis/internal/shopping_cart/injector"
+	transaction "Dzaakk/synapsis/internal/transaction/injector"
 
 	"github.com/gin-gonic/gin"
 )
@@ -19,5 +20,6 @@ func main() {
 	customer.InitializedService(db).Route(&r.RouterGroup)
 	product.InitializedService(db).Route(&r.RouterGroup)
 	shoppingCart.InitializedService(db).Route(&r.RouterGroup)
+	transaction.InitializedService(db).Route(&r.RouterGroup)
 	r.Run()
 }
