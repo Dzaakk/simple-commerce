3b867011601de22dbf0dcc8ca3606025dc72f584
Dzkzzz <dzdzaakk@gmail.com>
2024-10-14 20:41:44 +0700

    update default cost hash password

--- package/template/helper.go
+++ package/template/helper.go
@@ -8,7 +8,10 @@ import (
 )
 
 func HashPassword(password string) (string, error) {
-	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
 	return string(bytes), err
 }
 
