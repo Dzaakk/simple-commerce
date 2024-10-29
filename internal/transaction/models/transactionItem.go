0f3e8c396785a93d25114ee3ce2900b947875e27
Dzkzzz <dzdzaakk@gmail.com>
2024-10-29 22:07:33 +0700

    fix transaction item model, create transaction item repository

--- internal/transaction/models/transactionItem.go
+++ internal/transaction/models/transactionItem.go
@@ -3,11 +3,11 @@ package models
 import "Dzaakk/simple-commerce/package/template"
 
 type TTransactionItem struct {
-	Id          int64  `json:"id"`
-	CustomerId  int64  `json:"customer_id"`
-	ProductName string `json:"productName"`
-	Price       string `json:"price"`
-	Quantity    string `json:"quantity"`
-	Status      string `json:"status"`
+	Id          int64   `json:"id"`
+	CustomerId  int64   `json:"customer_id"`
+	ProductName string  `json:"productName"`
+	Price       float64 `json:"price"`
+	Quantity    int     `json:"quantity"`
+	Status      string  `json:"status"`
 	template.Base
 }
--- internal/transaction/repository/transaction_repository.go
+++ internal/transaction/repository/transaction_repository.go
@@ -1,10 +1,15 @@
 package repository
 
 import (
+	modelItem "Dzaakk/simple-commerce/internal/shopping_cart/models"
 	model "Dzaakk/simple-commerce/internal/transaction/models"
 )
 
 type TransactionRepository interface {
 	Create(data model.TTransaction) (*model.TTransaction, error)
 	FindByCustomerId(customerId int64)
+}
+
+type TransactionItemRepository interface {
+	Create(data []*modelItem.TCartItemDetail) error
 }
