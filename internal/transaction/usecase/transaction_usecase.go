836c823ab64397752ac3efcf36cb00b1879ef8fe
Dzkzzz <dzdzaakk@gmail.com>
2024-10-29 22:09:37 +0700

    move transaction item usecase, generate all method in transaction item

--- internal/transaction/usecase/transaction_usecase.go
+++ internal/transaction/usecase/transaction_usecase.go
@@ -1,9 +1,16 @@
 package usecase
 
-import model "Dzaakk/simple-commerce/internal/transaction/models"
+import (
+	model "Dzaakk/simple-commerce/internal/transaction/models"
+)
 
 type TransactionUseCase interface {
 	CreateTransaction(data model.TransactionReq) (*model.TransactionRes, error)
 	GetTransaction(customerId int64) ([]*model.CustomerListTransactionRes, error)
 	GetDetailTransaction(transactionId int64) ([]*model.CustomerListTransactionRes, error)
+}
+type TransactionItemUseCase interface {
+	GetListTransaction(customerId int)
+	GetTransactionItemDetail(transactionItemId int64)
+	CreateTransactionItemDetail(transactionItem model.TTransactionItem)
 }
