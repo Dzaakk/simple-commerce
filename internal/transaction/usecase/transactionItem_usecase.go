c0169808d45bdb54f0ac8373455c969e95fe7d94
Dzkzzz <dzdzaakk@gmail.com>
2024-10-29 22:06:49 +0700

    delete transaction item usecase join with transaction usecase

--- internal/transaction/usecase/transactionItem_usecase.go
+++
@@ -1,9 +0,0 @@
-package usecase
-
-import "Dzaakk/simple-commerce/internal/transaction/models"
-
-type TransactionItemUseCase interface {
-	GetListTransaction(customerId int)
-	GetTransactionItemDetail(transactionItemId int64)
-	CreateTransactionItemDetail(transactionItem models.TTransactionItem)
-}
