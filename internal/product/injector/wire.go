656b99f5c043e59b4ecf89c7a6afa87feb75c89b
Dzkzzz <dzdzaakk@gmail.com>
2024-11-14 22:16:35 +0700

    regenerate wire for product service

--- internal/product/injector/wire.go
+++ internal/product/injector/wire.go
@@ -4,10 +4,10 @@
 package injector
 
 import (
-	handler "Dzaakk/simple-commerce/internal/product/handlers"
-	repository "Dzaakk/simple-commerce/internal/product/repositories"
+	handlers "Dzaakk/simple-commerce/internal/product/handlers"
+	repositories "Dzaakk/simple-commerce/internal/product/repositories"
 	routes "Dzaakk/simple-commerce/internal/product/routes"
-	usecase "Dzaakk/simple-commerce/internal/product/usecases"
+	usecases "Dzaakk/simple-commerce/internal/product/usecases"
 	"database/sql"
 
 	"github.com/google/wire"
@@ -15,9 +15,9 @@ import (
 
 func InitializedService(db *sql.DB) *routes.ProductRoutes {
 	wire.Build(
-		repository.NewProductRepository,
-		usecase.NewProductUseCase,
-		handler.NewProductHandler,
+		repositories.NewProductRepository,
+		usecases.NewProductUseCase,
+		handlers.NewProductHandler,
 		routes.NewProductRoutes,
 	)
 
