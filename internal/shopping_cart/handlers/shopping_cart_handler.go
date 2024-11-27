664915d6bf4147d016012aa1092fc2ee81ae7dfc
Dzkzzz <dzdzaakk@gmail.com>
2024-11-27 23:26:36 +0700

    enhance response error in shopping cart handler

--- internal/shopping_cart/handlers/shopping_cart_handler.go
+++ internal/shopping_cart/handlers/shopping_cart_handler.go
@@ -3,6 +3,7 @@ package handler
 import (
 	model "Dzaakk/simple-commerce/internal/shopping_cart/models"
 	usecase "Dzaakk/simple-commerce/internal/shopping_cart/usecases"
+	"Dzaakk/simple-commerce/package/response"
 	template "Dzaakk/simple-commerce/package/templates"
 	"net/http"
 	"strconv"
@@ -23,18 +24,18 @@ func NewShoppingCartHandler(usecase usecase.ShoppingCartUseCase) *ShoppingCartHa
 func (handler *ShoppingCartHandler) AddProductToShoppingCart(ctx *gin.Context) {
 	var reqData model.ShoppingCartReq
 	if err := ctx.ShouldBindJSON(&reqData); err != nil {
-		ctx.JSON(http.StatusBadRequest, template.Response(http.StatusBadRequest, "Bad Request", "Invalid input data"))
+		ctx.JSON(http.StatusBadRequest, response.BadRequest("Invalid input data"))
 		return
 	}
 
 	template.AuthorizedChecker(ctx, reqData.CustomerId)
 	newShopingCart, err := handler.Usecase.Add(reqData)
 	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, template.Response(http.StatusInternalServerError, "Internal Server Error", err.Error()))
+		ctx.JSON(http.StatusInternalServerError, response.InternalServerError(err.Error()))
 		return
 	}
 
-	ctx.JSON(http.StatusCreated, template.Response(http.StatusOK, "Success", newShopingCart))
+	ctx.JSON(http.StatusOK, response.Success(newShopingCart))
 }
 
 func (handler *ShoppingCartHandler) GetListShoppingCart(ctx *gin.Context) {
@@ -44,30 +45,25 @@ func (handler *ShoppingCartHandler) GetListShoppingCart(ctx *gin.Context) {
 	if ctx.IsAborted() {
 		return
 	}
+
 	listShoppingCart, err := handler.Usecase.GetListItem(customerId)
 	if err != nil {
-
-		var statusCode int
-		var message string
 		if err.Error() == "cart is empty" {
-			statusCode = http.StatusOK
-			message = "your shopping cart is empty."
+			ctx.JSON(http.StatusOK, response.Success("your shopping cart is empty"))
+			return
 		} else {
-			statusCode = http.StatusInternalServerError
-			message = "Internal Server Error"
+			ctx.JSON(http.StatusInternalServerError, response.InternalServerError(err.Error()))
+			return
 		}
-
-		ctx.JSON(statusCode, template.Response(statusCode, message, err.Error()))
-		return
 	}
 
-	ctx.JSON(http.StatusCreated, template.Response(http.StatusOK, "Success", listShoppingCart))
+	ctx.JSON(http.StatusOK, response.Success(listShoppingCart))
 }
 
 func (handler *ShoppingCartHandler) DeleteShoppingList(ctx *gin.Context) {
 	var data model.DeleteReq
 	if err := ctx.ShouldBindJSON(&data); err != nil {
-		ctx.JSON(http.StatusBadRequest, template.Response(http.StatusBadRequest, "Bad Request", "Invalid input data"))
+		ctx.JSON(http.StatusBadRequest, response.BadRequest("Invalid input data"))
 		return
 	}
 	template.AuthorizedChecker(ctx, data.CustomerId)
@@ -77,9 +73,9 @@ func (handler *ShoppingCartHandler) DeleteShoppingList(ctx *gin.Context) {
 
 	err := handler.Usecase.DeleteShoppingList(data)
 	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, template.Response(http.StatusInternalServerError, "Internal Server Error", err.Error()))
+		ctx.JSON(http.StatusInternalServerError, response.InternalServerError(err.Error()))
 		return
 	}
 
-	ctx.JSON(http.StatusCreated, template.Response(http.StatusOK, "Success", "Success Delete Shopping List"))
+	ctx.JSON(http.StatusOK, response.Success("Success Delete Shopping List"))
 }
