23bc2c1de468acb0e39d5ce849c8d6855b90b068
Dzkzzz <dzdzaakk@gmail.com>
2024-11-29 10:43:24 +0700

    remove unnecessary tags

--- internal/product/models/product.go
+++ internal/product/models/product.go
@@ -3,11 +3,11 @@ package models
 import template "Dzaakk/simple-commerce/package/templates"
 
 type TProduct struct {
-	Id          int     `json:"id,omitempty,string"`
+	Id          int     `json:"id,omitempty"`
 	ProductName string  `json:"product_name"`
-	Price       float32 `json:"price,string"`
-	Stock       int     `json:"stock,string"`
-	CategoryId  int     `json:"category_id,string"`
-	SellerId    int     `json:"seller_id,string"`
+	Price       float32 `json:"price"`
+	Stock       int     `json:"stock"`
+	CategoryId  int     `json:"category_id"`
+	SellerId    int     `json:"seller_id"`
 	template.Base
 }
