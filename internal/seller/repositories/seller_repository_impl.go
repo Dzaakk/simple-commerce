e7d12afffb40986f1f0c181c4b190bd92061a1df
Dzkzzz <dzdzaakk@gmail.com>
2024-12-02 21:22:21 +0700

    create query const for seller repository

--- internal/seller/repositories/seller_repository_impl.go
+++ internal/seller/repositories/seller_repository_impl.go
@@ -15,6 +15,13 @@ func NewProductRepository(db *sql.DB) SellerRepository {
 	}
 }
 
+const (
+	queryCreateSeller  = "INSERT INTO public.seller (name, email, password, balance, created, created_by) VALUES ($1, $2, $3, $4, $5, $6)"
+	queryUpdateSeler   = "UPDATE public.seller SET name=$1, email=$2, password=$3, balance=$4, updated=NOW(), updated_by=$5 WHERE id=$6"
+	queryFindById      = "SELECT * FROM public.seller WHERE id = $1"
+	queryUpdateBalance = "UPDATE public.seller SET balance=$1, updated=NOW(), updated_by=$2 WHERE id=$3"
+)
+
 // Create implements SellerRepository.
 func (s *SellerRepositoryImpl) Create(models.SellerReq) error {
 	panic("unimplemented")
