a63ffcc22b35b07743ce72547818790f462e9c46
Dzkzzz <dzdzaakk@gmail.com>
2024-12-07 21:19:10 +0700

    implement seller update repository method and add deactivate method in seller usecase

--- internal/seller/repositories/seller_repository_impl.go
+++ internal/seller/repositories/seller_repository_impl.go
@@ -18,7 +18,7 @@ func NewProductRepository(db *sql.DB) SellerRepository {
 
 const (
 	queryCreateSeller  = "INSERT INTO public.seller (name, email, password, balance, created, created_by) VALUES ($1, $2, $3, $4, $5, $6)"
-	queryUpdateSeler   = "UPDATE public.seller SET name=$1, email=$2, password=$3, balance=$4, updated=NOW(), updated_by=$5 WHERE id=$6"
+	queryUpdateSeler   = "UPDATE public.seller SET name=$1, email=$2, password=$3, updated=NOW(), updated_by=$4 WHERE id=$5"
 	queryFindById      = "SELECT * FROM public.seller WHERE id = $1"
 	queryUpdateBalance = "UPDATE public.seller SET balance=$1, updated=NOW(), updated_by=$2 WHERE id=$3"
 )
@@ -38,9 +38,19 @@ func (repo *SellerRepositoryImpl) Create(data models.SellerReq) error {
 	return nil
 }
 
-// Update implements SellerRepository.
 func (repo *SellerRepositoryImpl) Update(data models.SellerReq) error {
-	panic("unimplemented")
+	statement, err := repo.DB.Prepare(queryUpdateSeler)
+	if err != nil {
+		return err
+	}
+	defer statement.Close()
+
+	_, err = statement.Exec(data.Name, data.Email, data.Password, time.Now(), data.Name)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // FindById implements SellerRepository.
--- internal/seller/usecases/seller_usecase.go
+++ internal/seller/usecases/seller_usecase.go
@@ -5,4 +5,5 @@ type SellerUseCase interface {
 	Update()
 	FindById()
 	FindByName()
+	Deactivate()
 }
