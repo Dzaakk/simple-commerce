90a432824806c5364271096de93896250e503070
Dzkzzz <dzdzaakk@gmail.com>
2024-12-16 21:26:45 +0700

    add seller id in product res, and create product req dto

--- internal/product/models/product_dto.go
+++ internal/product/models/product_dto.go
@@ -6,4 +6,14 @@ type ProductRes struct {
 	Price       string `json:"price,omitempty"`
 	Stock       string `json:"stock,omitempty"`
 	CategoryId  string `json:"category_id,omitempty"`
+	SellerId    string `json:"seller_id,omitempty"`
+}
+
+type ProductReq struct {
+	Id          string `json:"id,omitempty"`
+	ProductName string `json:"product_name,omitempty"`
+	Price       string `json:"price,omitempty"`
+	Stock       string `json:"stock,omitempty"`
+	CategoryId  string `json:"category_id,omitempty"`
+	SellerId    string `json:"seller_id,omitempty"`
 }
