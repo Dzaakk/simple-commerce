966b7b5490fa0805055a0a7637784d444b3cdb41
Dzkzzz <dzdzaakk@gmail.com>
2024-12-24 14:58:46 +0700

    implement find by seller id and category id, re use prepare in update method

--- internal/product/repositories/product_repository_impl.go
+++ internal/product/repositories/product_repository_impl.go
@@ -21,15 +21,16 @@ func NewProductRepository(db *sql.DB) ProductRepository {
 }
 
 const (
-	queryCreateProduct    = `INSERT INTO public.product (product_name, price, stock, category_id, seller_id, created, created_by) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id`
-	queryUpdate           = `UPDATE public.product SET product_name=$1, price=$2, stock=$3, updated=NOW(), updated_by=$4 WHERE id=$5`
-	queryFindByCategoryId = `SELECT * FROM public.product WHERE category_id = $1`
-	queryFindBySellerId   = `SELECT * FROM public.product WHERE seller_id = $1`
-	queryFindById         = `SELECT * FROM public.product WHERE id = $1`
-	queryGetPriceById     = `SELECT price FROM public.product WHERE id = $1`
-	queryGetStockById     = `SELECT stock FROM public.product WHERE id = $1`
-	queryFindByName       = `SELECT * FROM public.product WHERE product_name like '%' || $1 || '%'`
-	querySetStockById     = `UPDATE public.product SET stock = $1 WHERE id = $2`
+	queryCreateProduct               = `INSERT INTO public.product (product_name, price, stock, category_id, seller_id, created, created_by) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id`
+	queryUpdate                      = `UPDATE public.product SET product_name=$1, price=$2, stock=$3, updated=NOW(), updated_by=$4 WHERE id=$5`
+	queryFindByCategoryId            = `SELECT * FROM public.product WHERE category_id = $1`
+	queryFindBySellerIdAndCategoryId = `SELECT * FROM public.product WHERE seller_id = $1 AND category_id = $2`
+	queryFindBySellerId              = `SELECT * FROM public.product WHERE seller_id = $1`
+	queryFindById                    = `SELECT * FROM public.product WHERE id = $1`
+	queryGetPriceById                = `SELECT price FROM public.product WHERE id = $1`
+	queryGetStockById                = `SELECT stock FROM public.product WHERE id = $1`
+	queryFindByName                  = `SELECT * FROM public.product WHERE product_name like '%' || $1 || '%'`
+	querySetStockById                = `UPDATE public.product SET stock = $1 WHERE id = $2`
 )
 
 func (repo *ProductRepositoryImpl) Create(data model.TProduct) (*model.TProduct, error) {
@@ -51,13 +52,14 @@ func (repo *ProductRepositoryImpl) Create(data model.TProduct) (*model.TProduct,
 }
 
 func (repo *ProductRepositoryImpl) Update(data model.TProduct) (int64, error) {
-	result, err := repo.DB.Exec(
-		queryUpdate,
-		data.ProductName,
-		data.Price,
-		data.Stock,
-		data.UpdatedBy,
-		data.Id)
+	statement, err := repo.DB.Prepare(queryUpdate)
+	if err != nil {
+		return 0, err
+	}
+
+	defer statement.Close()
+
+	result, err := repo.DB.Exec(data.ProductName, data.Price, data.Stock, data.UpdatedBy, data.Id)
 
 	if err != nil {
 		return 0, err
@@ -90,12 +92,30 @@ func (repo *ProductRepositoryImpl) FindBySellerId(sellerId int) ([]*model.TProdu
 	return listProduct, nil
 }
 
-func (repo *ProductRepositoryImpl) FindBySellerIdAndCategoryId(sellerId int, categoryId int) ([]*model.TProduct, error) {
-	panic("unimplemented")
-}
-
 func (repo *ProductRepositoryImpl) FindByCategoryId(categoryId int) ([]*model.TProduct, error) {
 	rows, err := repo.DB.Query(queryFindByCategoryId, categoryId)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var listProduct []*model.TProduct
+	for rows.Next() {
+		product, err := retrieveProduct(rows)
+		if err != nil {
+			return nil, err
+		}
+		listProduct = append(listProduct, product)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return listProduct, nil
+}
+
+func (repo *ProductRepositoryImpl) FindBySellerIdAndCategoryId(sellerId int, categoryId int) ([]*model.TProduct, error) {
+	rows, err := repo.DB.Query(queryFindBySellerIdAndCategoryId, sellerId, categoryId)
 	if err != nil {
 		return nil, err
 	}
