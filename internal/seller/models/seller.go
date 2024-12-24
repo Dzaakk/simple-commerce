5d0b2c934cc90904021f33d748a8aa0849829e5b
Dzkzzz <dzdzaakk@gmail.com>
2024-12-24 15:00:00 +0700

    change seller name to username, create dto

--- internal/seller/models/seller.go
+++ internal/seller/models/seller.go
@@ -4,20 +4,9 @@ import "Dzaakk/simple-commerce/package/templates"
 
 type TSeller struct {
 	Id       int64   `json:"id"`
-	Name     string  `json:"name"`
+	Username string  `json:"username"`
 	Email    string  `json:"email"`
 	Password string  `json:"password"`
 	Balance  float64 `json:"balance"`
 	templates.Base
-}
-
-type SellerReq struct {
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
-
-type SellerRes struct {
-	Name  string `json:"name"`
-	Email string `json:"email"`
 }
---
+++ internal/seller/models/seller_dto.go
@@ -0,0 +1,25 @@
+package models
+
+type ReqCreate struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+type ReqUpdate struct {
+	Id       string `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type ResCreate struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+}
+
+type ResData struct {
+	Id       string `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Balance  string `json:"balance"`
+}
