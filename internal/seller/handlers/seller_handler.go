e0e926d834845aefb6c8d5f5375f9f7e190519d7
Dzkzzz <dzdzaakk@gmail.com>
2025-01-08 22:11:48 +0700

    initiate seller method handler

--- internal/seller/handlers/seller_handler.go
+++ internal/seller/handlers/seller_handler.go
@@ -1,1 +1,31 @@
 package handlers
+
+import (
+	usecase "Dzaakk/simple-commerce/internal/seller/usecases"
+
+	"github.com/gin-gonic/gin"
+)
+
+type SellerHandler struct {
+	Usecase usecase.SellerUseCase
+}
+
+func NewSellerHandler(usecase usecase.SellerUseCase) *SellerHandler {
+	return &SellerHandler{Usecase: usecase}
+}
+
+func (handler *SellerHandler) Register(ctx *gin.Context) {
+
+}
+func (handler *SellerHandler) Login(ctx *gin.Context) {
+
+}
+func (handler *SellerHandler) FindById(ctx *gin.Context) {
+
+}
+func (handler *SellerHandler) Deactivate(ctx *gin.Context) {
+
+}
+func (handler *SellerHandler) ChangePassword(ctx *gin.Context) {
+
+}
