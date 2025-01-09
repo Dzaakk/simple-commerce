7431f50da676f9601602868cd45c4c3a7681588e
Dzkzzz <dzdzaakk@gmail.com>
2025-01-09 23:05:46 +0700

    add ctx to param all method

--- internal/customer/repositories/customer_repository.go
+++ internal/customer/repositories/customer_repository.go
@@ -2,18 +2,19 @@ package repositories
 
 import (
 	model "Dzaakk/simple-commerce/internal/customer/models"
+	"context"
 	"database/sql"
 )
 
 type CustomerRepository interface {
-	Create(data model.TCustomers) (int64, error)
-	FindById(id int64) (*model.TCustomers, error)
-	UpdateBalance(id int64, balance float64) (float64, error)
-	GetBalance(id int64) (*model.CustomerBalance, error)
-	InquiryBalance(id int64) (float64, error)
-	FindByEmail(email string) (*model.TCustomers, error)
-	GetBalanceWithTx(tx *sql.Tx, id int64) (*model.CustomerBalance, error)
-	UpdateBalanceWithTx(tx *sql.Tx, id int64, newBalance float64) error
-	UpdatePassword(id int64, newPassword string) (int64, error)
-	Deactive(id int64) (int64, error)
+	Create(ctx context.Context, data model.TCustomers) (int64, error)
+	FindById(ctx context.Context, id int64) (*model.TCustomers, error)
+	UpdateBalance(ctx context.Context, id int64, balance float64) (float64, error)
+	GetBalance(ctx context.Context, id int64) (*model.CustomerBalance, error)
+	InquiryBalance(ctx context.Context, id int64) (float64, error)
+	FindByEmail(ctx context.Context, email string) (*model.TCustomers, error)
+	GetBalanceWithTx(ctx context.Context, tx *sql.Tx, id int64) (*model.CustomerBalance, error)
+	UpdateBalanceWithTx(ctx context.Context, tx *sql.Tx, id int64, newBalance float64) error
+	UpdatePassword(ctx context.Context, id int64, newPassword string) (int64, error)
+	Deactive(ctx context.Context, id int64) (int64, error)
 }
