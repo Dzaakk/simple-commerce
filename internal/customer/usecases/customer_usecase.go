19d0c612e35c30240872c5c1697cd81f2129af23
Dzkzzz <dzdzaakk@gmail.com>
2025-01-12 23:35:54 +0700

    add context to all method parameter

--- internal/customer/usecases/customer_usecase.go
+++ internal/customer/usecases/customer_usecase.go
@@ -2,18 +2,19 @@ package usecases
 
 import (
 	model "Dzaakk/simple-commerce/internal/customer/models"
+	"context"
 )
 
 type CustomerUseCase interface {
-	Create(data model.CreateReq) (int64, error)
-	FindById(id int64) (*model.DataRes, error)
-	UpdateBalance(id int64, balance float64, actionType string) (float64, error)
-	GetBalance(id int64) (*model.CustomerBalanceRes, error)
-	IncreaseBalance(id int64, amount float64) (*model.CustomerBalanceRes, error)
-	DecreaseBalance(id int64, amount float64) (*model.CustomerBalanceRes, error)
-	FindByEmail(email string) (*model.TCustomers, error)
-	Update(data model.UpdateReq) (int64, error)
-	UpdatePassword(id int64, newPassword string) (int64, error)
-	Deactivate(id int64) (int64, error)
+	Create(ctx context.Context, data model.CreateReq) (int64, error)
+	FindById(ctx context.Context, id int64) (*model.DataRes, error)
+	UpdateBalance(ctx context.Context, id int64, balance float64, actionType string) (float64, error)
+	GetBalance(ctx context.Context, id int64) (*model.CustomerBalanceRes, error)
+	IncreaseBalance(ctx context.Context, id int64, amount float64) (*model.CustomerBalanceRes, error)
+	DecreaseBalance(ctx context.Context, id int64, amount float64) (*model.CustomerBalanceRes, error)
+	FindByEmail(ctx context.Context, email string) (*model.TCustomers, error)
+	Update(ctx context.Context, data model.UpdateReq) (int64, error)
+	UpdatePassword(ctx context.Context, id int64, newPassword string) (int64, error)
+	Deactivate(ctx context.Context, id int64) (int64, error)
 	//inquiry balance
 }
