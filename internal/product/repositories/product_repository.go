567691432d8889853676f3dddf13ec1dc9e5a731
Dzkzzz <dzdzaakk@gmail.com>
2025-01-14 21:35:18 +0700

    add context in all method

--- internal/product/repositories/product_repository.go
+++ internal/product/repositories/product_repository.go
@@ -3,22 +3,23 @@ package repositories
 import (
 	model "Dzaakk/simple-commerce/internal/product/models"
 	modelCart "Dzaakk/simple-commerce/internal/shopping_cart/models"
+	"context"
 	"database/sql"
 )
 
 type ProductRepository interface {
-	Create(data model.TProduct) (*model.TProduct, error)
-	Update(data model.TProduct) (int64, error)
-	FindByCategoryId(categoryId int) ([]*model.TProduct, error)
-	FindById(id int) (*model.TProduct, error)
-	FindByName(name string) (*model.TProduct, error)
-	SetStockById(id int, stock int) (int64, error)
-	GetPriceById(id int) (float32, error)
-	GetStockById(id int) (int, error)
-	UpdateStock(listData []*modelCart.TCartItemDetail, name string) error
-	UpdateStockWithTx(tx *sql.Tx, listData []*modelCart.TCartItemDetail) ([]*int, error)
+	Create(ctx context.Context, data model.TProduct) (*model.TProduct, error)
+	Update(ctx context.Context, data model.TProduct) (int64, error)
+	FindByCategoryId(ctx context.Context, categoryId int) ([]*model.TProduct, error)
+	FindById(ctx context.Context, id int) (*model.TProduct, error)
+	FindByName(ctx context.Context, name string) (*model.TProduct, error)
+	SetStockById(ctx context.Context, id int, stock int) (int64, error)
+	GetPriceById(ictx context.Context, d int) (float32, error)
+	GetStockById(ctx context.Context, id int) (int, error)
+	UpdateStock(ctx context.Context, listData []*modelCart.TCartItemDetail, name string) error
+	UpdateStockWithTx(ctx context.Context, tx *sql.Tx, listData []*modelCart.TCartItemDetail) ([]*int, error)
 
 	//Seller
-	FindBySellerId(sellerId int) ([]*model.TProduct, error)
-	FindBySellerIdAndCategoryId(sellerId, categoryId int) ([]*model.TProduct, error)
+	FindBySellerId(ctx context.Context, sellerId int) ([]*model.TProduct, error)
+	FindBySellerIdAndCategoryId(ctx context.Context, sellerId, categoryId int) ([]*model.TProduct, error)
 }
