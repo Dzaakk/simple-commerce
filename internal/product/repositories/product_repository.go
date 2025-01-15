8a6b232d4b22050bee113bc82bd349fc23d8b5c4
Dzkzzz <dzdzaakk@gmail.com>
2025-01-15 22:48:51 +0700

    remove redundant method and make one dynamic method for find products

--- internal/product/repositories/product_repository.go
+++ internal/product/repositories/product_repository.go
@@ -10,7 +10,6 @@ import (
 type ProductRepository interface {
 	Create(ctx context.Context, data model.TProduct) (*model.TProduct, error)
 	Update(ctx context.Context, data model.TProduct) (int64, error)
-	FindByCategoryId(ctx context.Context, categoryId int) ([]*model.TProduct, error)
 	FindById(ctx context.Context, id int) (*model.TProduct, error)
 	FindByName(ctx context.Context, name string) (*model.TProduct, error)
 	SetStockById(ctx context.Context, id int, stock int) (int64, error)
@@ -18,8 +17,5 @@ type ProductRepository interface {
 	GetStockById(ctx context.Context, id int) (int, error)
 	UpdateStock(ctx context.Context, listData []*modelCart.TCartItemDetail, name string) error
 	UpdateStockWithTx(ctx context.Context, tx *sql.Tx, listData []*modelCart.TCartItemDetail) ([]*int, error)
-
-	//Seller
-	FindBySellerId(ctx context.Context, sellerId int) ([]*model.TProduct, error)
-	FindBySellerIdAndCategoryId(ctx context.Context, sellerId, categoryId int) ([]*model.TProduct, error)
+	FindProductByFilters(ctx context.Context, sellerId, CategoryId *int) ([]*model.TProduct, error)
 }
