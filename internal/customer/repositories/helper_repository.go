d08e86502f7e966696c1db4ce71affdbd0c9d251
Dzkzzz <dzdzaakk@gmail.com>
2025-01-16 23:02:57 +0700

    create helper repository

---
+++ internal/customer/repositories/helper_repository.go
@@ -0,0 +1,35 @@
+package repositories
+
+import (
+	model "Dzaakk/simple-commerce/internal/customer/models"
+	template "Dzaakk/simple-commerce/package/templates"
+	"database/sql"
+	"errors"
+	"fmt"
+)
+
+func scanCustomer(row *sql.Row) (*model.TCustomers, error) {
+	customer := &model.TCustomers{}
+	base := template.Base{}
+	var updated sql.NullTime
+
+	err := row.Scan(
+		&customer.Id, &customer.Username, &customer.Email, &customer.Password, &customer.PhoneNumber, &customer.Balance, &customer.Status,
+		&base.Created, &base.CreatedBy, &updated, &base.UpdatedBy)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, nil
+		}
+		return nil, fmt.Errorf("failed to scan customer: %w", err)
+	}
+	if updated.Valid {
+		base.Updated.Time = updated.Time
+	}
+	if !base.UpdatedBy.Valid {
+		base.UpdatedBy.String = ""
+	}
+
+	customer.Base = base
+
+	return customer, nil
+}
