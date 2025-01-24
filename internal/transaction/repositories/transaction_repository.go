7d369e3eefb072bbe0a6b6946b687165d8d1b56c
Dzkzzz <dzdzaakk@gmail.com>
2025-01-24 22:45:35 +0700

    add context in all method param, and implement context in query method

--- internal/transaction/repositories/transaction_repository.go
+++ internal/transaction/repositories/transaction_repository.go
@@ -3,17 +3,18 @@ package repository
 import (
 	modelItem "Dzaakk/simple-commerce/internal/shopping_cart/models"
 	model "Dzaakk/simple-commerce/internal/transaction/models"
+	"context"
 	"database/sql"
 )
 
 type TransactionRepository interface {
-	Create(data model.TTransaction) (*model.TTransaction, error)
-	CreateWithTx(tx *sql.Tx, data model.TTransaction) (*model.TTransaction, error)
-	FindByCustomerId(customerId int64)
+	Create(ctx context.Context, data model.TTransaction) (*model.TTransaction, error)
+	CreateWithTx(ctx context.Context, tx *sql.Tx, data model.TTransaction) (*model.TTransaction, error)
+	FindByCustomerId(ctx context.Context, customerId int64)
 	BeginTransaction() (*sql.Tx, error)
 }
 
 type HistoryTransactionRepository interface {
-	Create(data []*modelItem.TCartItemDetail, customerId int64) error
-	FindByCustomerId(customerId int64) ([]*model.THistoryTransaction, error)
+	Create(ctx context.Context, data []*modelItem.TCartItemDetail, customerId int64) error
+	FindByCustomerId(ctx context.Context, customerId int64) ([]*model.THistoryTransaction, error)
 }
--- internal/transaction/repositories/transaction_repository_impl.go
+++ internal/transaction/repositories/transaction_repository_impl.go
@@ -2,6 +2,8 @@ package repository
 
 import (
 	model "Dzaakk/simple-commerce/internal/transaction/models"
+	response "Dzaakk/simple-commerce/package/response"
+	"context"
 	"database/sql"
 )
 
@@ -17,22 +19,17 @@ func NewTransactionRepository(db *sql.DB) TransactionRepository {
 
 const queryCreateTransaction = `INSERT INTO public.transaction (customer_id, cart_id, total_amount, transaction_date, status, created, created_by) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id`
 
-func (repo *TransactionRepositoryImpl) Create(data model.TTransaction) (*model.TTransaction, error) {
-	statement, err := repo.DB.Prepare(queryCreateTransaction)
-	if err != nil {
-		return nil, err
-	}
-	defer statement.Close()
+func (repo *TransactionRepositoryImpl) Create(ctx context.Context, data model.TTransaction) (*model.TTransaction, error) {
 
-	var id int
-
-	err = statement.QueryRow(data.CustomerId, data.CartId, data.TotalAmount, data.TransactionDate, data.Status, data.Base.Created, data.Base.CreatedBy).Scan(&id)
+	result, err := repo.DB.ExecContext(ctx, queryCreateTransaction, data.CustomerId, data.CartId, data.TotalAmount, data.TransactionDate, data.Status, data.Base.Created, data.Base.CreatedBy)
 	if err != nil {
-		return nil, err
+		return nil, response.ExecError("create transaction", err)
 	}
+	id, _ := result.LastInsertId()
+	data.Id = int(id)
 	return &data, nil
 }
-func (repo *TransactionRepositoryImpl) CreateWithTx(tx *sql.Tx, data model.TTransaction) (*model.TTransaction, error) {
+func (repo *TransactionRepositoryImpl) CreateWithTx(ctx context.Context, tx *sql.Tx, data model.TTransaction) (*model.TTransaction, error) {
 	statement, err := tx.Prepare(queryCreateTransaction)
 	if err != nil {
 		return nil, err
@@ -52,6 +49,6 @@ func (repo *TransactionRepositoryImpl) BeginTransaction() (*sql.Tx, error) {
 	return repo.DB.Begin()
 }
 
-func (repo *TransactionRepositoryImpl) FindByCustomerId(customerId int64) {
+func (repo *TransactionRepositoryImpl) FindByCustomerId(ctx context.Context, customerId int64) {
 	panic("unimplemented")
 }
