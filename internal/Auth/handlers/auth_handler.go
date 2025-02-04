6fd77917f2a6b3649692ed8f3c552c5bb2c0d130
Dzkzzz <dzdzaakk@gmail.com>
2025-02-04 18:02:42 +0700

    move login and register customer handler to auth handler

--- internal/Auth/handlers/auth_handler.go
+++ internal/Auth/handlers/auth_handler.go
@@ -1,1 +1,45 @@
 package handlers
+
+type AuthHandler struct {
+}
+
+// func (handler *CustomerHandler) Login(ctx *gin.Context) {
+// 	var reqData model.LoginReq
+
+// 	if err := ctx.ShouldBindJSON(&reqData); err != nil {
+// 		ctx.JSON(http.StatusBadRequest, response.BadRequest("Invalid email or password"))
+// 		return
+// 	}
+
+// 	data, err := handler.Usecase.FindByEmail(ctx, reqData.Email)
+// 	if err != nil {
+// 		ctx.JSON(http.StatusInternalServerError, response.InternalServerError(err.Error()))
+// 		return
+// 	}
+// 	if !template.CheckPasswordHash(reqData.Password, data.Password) {
+// 		ctx.JSON(http.StatusBadRequest, response.BadRequest("Invalid email or password"))
+// 		return
+// 	}
+// 	// cache token
+// 	// _, err = auth.NewTokenGenerator(db.Redis(), *data)
+// 	if err != nil {
+// 		ctx.JSON(http.StatusInternalServerError, response.InternalServerError(err.Error()))
+// 		return
+// 	}
+// 	ctx.JSON(http.StatusOK, response.Success("Login Success"))
+// }
+
+// func (handler *CustomerHandler) Create(ctx *gin.Context) {
+// 	var data model.CreateReq
+
+// 	if err := ctx.ShouldBindJSON(&data); err != nil {
+// 		ctx.JSON(http.StatusBadRequest, response.BadRequest("Invalid input data"))
+// 		return
+// 	}
+// 	_, err := handler.Usecase.Create(ctx, data)
+// 	if err != nil {
+// 		ctx.JSON(http.StatusInternalServerError, response.InternalServerError(err.Error()))
+// 		return
+// 	}
+// 	ctx.JSON(http.StatusOK, response.Success("Success Create User"))
+// }
--- internal/customer/handlers/customer_handler.go
+++ internal/customer/handlers/customer_handler.go
@@ -22,32 +22,6 @@ func NewCustomerHandler(usecase usecase.CustomerUseCase) *CustomerHandler {
 	}
 }
 
-func (handler *CustomerHandler) Login(ctx *gin.Context) {
-	var reqData model.LoginReq
-
-	if err := ctx.ShouldBindJSON(&reqData); err != nil {
-		ctx.JSON(http.StatusBadRequest, response.BadRequest("Invalid email or password"))
-		return
-	}
-
-	data, err := handler.Usecase.FindByEmail(ctx, reqData.Email)
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, response.InternalServerError(err.Error()))
-		return
-	}
-	if !template.CheckPasswordHash(reqData.Password, data.Password) {
-		ctx.JSON(http.StatusBadRequest, response.BadRequest("Invalid email or password"))
-		return
-	}
-	// cache token
-	// _, err = auth.NewTokenGenerator(db.Redis(), *data)
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, response.InternalServerError(err.Error()))
-		return
-	}
-	ctx.JSON(http.StatusOK, response.Success("Login Success"))
-}
-
 func (handler *CustomerHandler) FindCustomerById(ctx *gin.Context) {
 	id, err := strconv.ParseInt(ctx.Query("id"), 10, 64)
 	if err != nil {
@@ -65,21 +39,6 @@ func (handler *CustomerHandler) FindCustomerById(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusOK, response.Success(data))
-}
-
-func (handler *CustomerHandler) Create(ctx *gin.Context) {
-	var data model.CreateReq
-
-	if err := ctx.ShouldBindJSON(&data); err != nil {
-		ctx.JSON(http.StatusBadRequest, response.BadRequest("Invalid input data"))
-		return
-	}
-	_, err := handler.Usecase.Create(ctx, data)
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, response.InternalServerError(err.Error()))
-		return
-	}
-	ctx.JSON(http.StatusOK, response.Success("Success Create User"))
 }
 
 func (handler *CustomerHandler) UpdateBalance(ctx *gin.Context) {
