0b2c69e6bce002a361875fc3d514e4e2f5ff58f3
Dzkzzz <dzdzaakk@gmail.com>
2025-02-09 23:17:30 +0700

    implement customer registration

--- internal/Auth/usecases/auth_usecase.go
+++ internal/Auth/usecases/auth_usecase.go
@@ -1,6 +1,11 @@
 package usecases
 
+import (
+	model "Dzaakk/simple-commerce/internal/Auth/models"
+	"context"
+)
+
 type AuthUseCase interface {
-	CustomerRegistration()
+	CustomerRegistration(ctx context.Context, data model.CustomerRegistration) (int64, error)
 	CustomerLogin()
 }
--- internal/Auth/usecases/auth_usecase_impl.go
+++ internal/Auth/usecases/auth_usecase_impl.go
@@ -1,7 +1,12 @@
 package usecases
 
 import (
+	model "Dzaakk/simple-commerce/internal/Auth/models"
+	customerModel "Dzaakk/simple-commerce/internal/customer/models"
 	customerRepo "Dzaakk/simple-commerce/internal/customer/repositories"
+	template "Dzaakk/simple-commerce/package/templates"
+	"context"
+	"time"
 )
 
 type AuthUseCaseImpl struct {
@@ -12,12 +17,33 @@ func NewAuthUseCase(customerRepo customerRepo.CustomerRepository) AuthUseCase {
 	return &AuthUseCaseImpl{customerRepo}
 }
 
-// CustomerLogin implements AuthUseCase.
-func (a *AuthUseCaseImpl) CustomerLogin() {
-	panic("unimplemented")
+func (a *AuthUseCaseImpl) CustomerRegistration(ctx context.Context, data model.CustomerRegistration) (int64, error) {
+
+	hashedPassword, err := template.HashPassword(data.Password)
+	if err != nil {
+		return 0, err
+	}
+
+	customer := customerModel.TCustomers{
+		Username:    data.Username,
+		Email:       data.Email,
+		PhoneNumber: data.PhoneNumber,
+		Password:    string(hashedPassword),
+		Balance:     float64(10000000),
+		Status:      "A",
+		Base: template.Base{
+			Created:   time.Now(),
+			CreatedBy: "system",
+		},
+	}
+
+	customerId, err := a.customerRepo.Create(ctx, customer)
+	if err != nil {
+		return 0, err
+	}
+	return customerId, nil
 }
 
-// CustomerRegistration implements AuthUseCase.
-func (a *AuthUseCaseImpl) CustomerRegistration() {
+func (a *AuthUseCaseImpl) CustomerLogin() {
 	panic("unimplemented")
 }
