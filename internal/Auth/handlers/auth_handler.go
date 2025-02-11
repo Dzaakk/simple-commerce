fecfbe88cbff362a7d1af369eaa05a624b3dfd22
Dzkzzz <dzdzaakk@gmail.com>
2025-02-11 22:27:25 +0700

    reposition login and regist customer, create handler for regist and login seller

--- internal/Auth/handlers/auth_handler.go
+++ internal/Auth/handlers/auth_handler.go
@@ -23,7 +23,22 @@ func NewAtuhHandler(usecase usecase.AuthUseCase, custUsecase custUsecase.Custome
 	}
 }
 
-func (h *AuthHandler) Login(ctx *gin.Context) {
+func (h *AuthHandler) RegistrationCustomer(ctx *gin.Context) {
+	var data model.CustomerRegistration
+
+	if err := ctx.ShouldBindJSON(&data); err != nil {
+		ctx.JSON(http.StatusBadRequest, response.BadRequest("Invalid input data"))
+		return
+	}
+	_, err := h.Usecase.CustomerRegistration(ctx, data)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, response.InternalServerError(err.Error()))
+		return
+	}
+	ctx.JSON(http.StatusOK, response.Success("Success Create User"))
+}
+
+func (h *AuthHandler) LoginCustomer(ctx *gin.Context) {
 	var reqData model.LoginReq
 
 	if err := ctx.ShouldBindJSON(&reqData); err != nil {
@@ -50,17 +65,5 @@ func (h *AuthHandler) Login(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, response.Success("Login Success"))
 }
 
-func (h *AuthHandler) RegistrationCustomer(ctx *gin.Context) {
-	var data model.CustomerRegistration
-
-	if err := ctx.ShouldBindJSON(&data); err != nil {
-		ctx.JSON(http.StatusBadRequest, response.BadRequest("Invalid input data"))
-		return
-	}
-	_, err := h.Usecase.CustomerRegistration(ctx, data)
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, response.InternalServerError(err.Error()))
-		return
-	}
-	ctx.JSON(http.StatusOK, response.Success("Success Create User"))
-}
+func (h *AuthHandler) RegistrationSeller(ctx *gin.Context) {}
+func (h *AuthHandler) LoginSeller(ctx *gin.Context)        {}
