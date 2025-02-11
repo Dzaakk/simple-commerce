77aa8f20ff6215ae95bb429efa3a10123bb9212d
Dzkzzz <dzdzaakk@gmail.com>
2025-02-11 22:27:47 +0700

    create route for handling regist and login for customer and seller

--- internal/Auth/routes/auth_route.go
+++ internal/Auth/routes/auth_route.go
@@ -1,1 +1,30 @@
 package routes
+
+import (
+	handler "Dzaakk/simple-commerce/internal/auth/handlers"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v8"
+)
+
+type AuthRoutes struct {
+	Handler *handler.AuthHandler
+}
+
+func NewAuthRoutes(handler *handler.AuthHandler) *AuthRoutes {
+	return &AuthRoutes{
+		Handler: handler,
+	}
+}
+
+func (ar *AuthRoutes) Route(r *gin.RouterGroup, redis *redis.Client) {
+	authHandler := r.Group("/api/v1")
+
+	authHandler.Use()
+	{
+		authHandler.POST("/login-customer", ar.Handler.LoginCustomer)
+		authHandler.POST("/register-customer", ar.Handler.RegistrationCustomer)
+		authHandler.POST("/login-seller", ar.Handler.LoginSeller)
+		authHandler.POST("/register-seller", ar.Handler.RegistrationSeller)
+	}
+}
