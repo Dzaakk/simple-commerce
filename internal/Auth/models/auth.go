3dd5a2a7cd684b5380550a77a9838dbd691daa24
Dzkzzz <dzdzaakk@gmail.com>
2025-02-12 21:47:17 +0700

    create new model for seller registration

--- internal/Auth/models/auth.go
+++ internal/Auth/models/auth.go
@@ -11,3 +11,9 @@ type CustomerRegistration struct {
 	PhoneNumber string `json:"phone_number" validate:"required"`
 	Password    string `json:"password" validate:"required"`
 }
+
+type SellerRegistration struct {
+	Username string `json:"username" validate:"required"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
+}
