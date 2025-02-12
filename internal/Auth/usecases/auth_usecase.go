efbcef6839710887166111cf94480f5ab7e7c76c
Dzkzzz <dzdzaakk@gmail.com>
2025-02-12 21:47:53 +0700

    add login and regist methods for seller

--- internal/Auth/usecases/auth_usecase.go
+++ internal/Auth/usecases/auth_usecase.go
@@ -6,6 +6,8 @@ import (
 )
 
 type AuthUseCase interface {
-	CustomerRegistration(ctx context.Context, data model.CustomerRegistration) (int64, error)
+	CustomerRegistration(ctx context.Context, data model.CustomerRegistration) (*int64, error)
 	CustomerLogin()
+	SellerRegistration(ctx context.Context, data model.SellerRegistration) (*int64, error)
+	SellerLogin()
 }
