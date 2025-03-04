79f0873f38281f308f664aa7a9c63ddb4d177c99
Dzkzzz <dzdzaakk@gmail.com>
2025-03-04 22:56:47 +0700

    create auth repository method

--- internal/Auth/repositories/auth_repository.go
+++ internal/Auth/repositories/auth_repository.go
@@ -1,1 +1,11 @@
 package repositories
+
+import (
+	model "Dzaakk/simple-commerce/internal/auth/models"
+	"context"
+)
+
+type AuthRepository interface {
+	CreateCode(ctx context.Context, data model.TCodeActivation) error
+	FindCodeByCustomerId(ctx context.Context, id int64) (*model.TCodeActivation, error)
+}
--- internal/Auth/repositories/auth_repository_impl.go
+++ internal/Auth/repositories/auth_repository_impl.go
@@ -1,1 +1,25 @@
 package repositories
+
+import (
+	"Dzaakk/simple-commerce/internal/auth/models"
+	"context"
+	"database/sql"
+)
+
+type AuthRepositoryImpl struct {
+	DB *sql.DB
+}
+
+func NewAuthRepository(db *sql.DB) AuthRepository {
+	return &AuthRepositoryImpl{DB: db}
+}
+
+// CreateCode implements AuthRepository.
+func (a *AuthRepositoryImpl) CreateCode(ctx context.Context, data models.TCodeActivation) error {
+	panic("unimplemented")
+}
+
+// FindCodeByCustomerId implements AuthRepository.
+func (a *AuthRepositoryImpl) FindCodeByCustomerId(ctx context.Context, id int64) (*models.TCodeActivation, error) {
+	panic("unimplemented")
+}
