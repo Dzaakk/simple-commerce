a8d294ae4fbda8d85d5c3ef259e8cfdd56b71e7c
Dzkzzz <dzdzaakk@gmail.com>
2025-03-05 22:07:09 +0700

    implement create repo method

--- internal/Auth/repositories/auth_repository_impl.go
+++ internal/Auth/repositories/auth_repository_impl.go
@@ -2,8 +2,10 @@ package repositories
 
 import (
 	"Dzaakk/simple-commerce/internal/auth/models"
+	response "Dzaakk/simple-commerce/package/response"
 	"context"
 	"database/sql"
+	"time"
 )
 
 type AuthRepositoryImpl struct {
@@ -14,9 +16,25 @@ func NewAuthRepository(db *sql.DB) AuthRepository {
 	return &AuthRepositoryImpl{DB: db}
 }
 
-// CreateCode implements AuthRepository.
-func (a *AuthRepositoryImpl) CreateCode(ctx context.Context, data models.TCodeActivation) error {
-	panic("unimplemented")
+const (
+	queryCreate    = `INSERT INTO public.code_activation (user_id, code_activation, is_used, created_at, used_at) VALUES ($1, $2, $3, $4, $5)`
+	dbQueryTimeout = 3 * time.Second
+)
+
+func (repo *AuthRepositoryImpl) contextWithTimeout(ctx context.Context) (context.Context, context.CancelFunc) {
+	return context.WithTimeout(ctx, dbQueryTimeout)
+}
+
+func (repo *AuthRepositoryImpl) CreateCode(c context.Context, data models.TCodeActivation) error {
+	ctx, cancel := repo.contextWithTimeout(c)
+	defer cancel()
+
+	_, err := repo.DB.ExecContext(ctx, queryCreate, data.UserID, data.CodeActivation, data.IsUsed, data.CreatedAt, data.UsedAt)
+	if err != nil {
+		return response.ExecError("create activation code", err)
+	}
+
+	return nil
 }
 
 // FindCodeByCustomerId implements AuthRepository.
