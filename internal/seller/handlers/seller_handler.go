927c69eee4c76e9191efcefbff79dbd65507075d
Dzkzzz <dzdzaakk@gmail.com>
2025-03-21 23:39:48 +0700

    remove register and login handler, implement find by username handler

--- internal/seller/handlers/seller_handler.go
+++ internal/seller/handlers/seller_handler.go
@@ -2,6 +2,8 @@ package handlers
 
 import (
 	usecase "Dzaakk/simple-commerce/internal/seller/usecases"
+	"Dzaakk/simple-commerce/package/response"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -14,13 +16,21 @@ func NewSellerHandler(usecase usecase.SellerUseCase) *SellerHandler {
 	return &SellerHandler{Usecase: usecase}
 }
 
-func (handler *SellerHandler) Register(ctx *gin.Context) {
-
-}
-func (handler *SellerHandler) Login(ctx *gin.Context) {
-
-}
 func (handler *SellerHandler) FindById(ctx *gin.Context) {
+
+}
+func (handler *SellerHandler) FindByUsername(ctx *gin.Context) {
+	username := ctx.Query("username")
+	if username != "" {
+		seller, err := handler.Usecase.FindByUsername(ctx, username)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, response.InternalServerError(err.Error()))
+			return
+		}
+		ctx.JSON(http.StatusOK, response.Success(seller))
+	}
+}
+func (handler *SellerHandler) FindAll(ctx *gin.Context) {
 
 }
 func (handler *SellerHandler) Deactivate(ctx *gin.Context) {
