6f4e28168d11971af99aec34e389322ac4593d5f
Dzkzzz <dzdzaakk@gmail.com>
2025-03-21 23:38:52 +0700

    create helper for convert list seller data

---
+++ internal/seller/usecases/helper_usecase.go
@@ -0,0 +1,20 @@
+package usecases
+
+import (
+	model "Dzaakk/simple-commerce/internal/seller/models"
+	"strconv"
+)
+
+func ConvertSellersToResData(sellers []*model.TSeller) []*model.ResData {
+	resDataList := make([]*model.ResData, len(sellers))
+	for i, seller := range sellers {
+		resData := &model.ResData{
+			Id:       strconv.FormatInt(seller.Id, 10),
+			Username: seller.Username,
+			Email:    seller.Email,
+			Balance:  strconv.FormatFloat(seller.Balance, 'f', -1, 64),
+		}
+		resDataList[i] = resData
+	}
+	return resDataList
+}
