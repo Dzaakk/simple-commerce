a920155999627c9c75408091f24b1b1577f96517
Dzkzzz <dzdzaakk@gmail.com>
2025-03-22 22:55:57 +0700

    implement find by id

--- internal/seller/handlers/seller_handler.go
+++ internal/seller/handlers/seller_handler.go
@@ -4,6 +4,7 @@ import (
 	usecase "Dzaakk/simple-commerce/internal/seller/usecases"
 	"Dzaakk/simple-commerce/package/response"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -17,8 +18,18 @@ func NewSellerHandler(usecase usecase.SellerUseCase) *SellerHandler {
 }
 
 func (handler *SellerHandler) FindById(ctx *gin.Context) {
+	id, _ := strconv.Atoi(ctx.Query("id"))
 
+	if id != 0 {
+		seller, err := handler.Usecase.FindById(ctx, int64(id))
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, response.InternalServerError(err.Error()))
+			return
+		}
+		ctx.JSON(http.StatusOK, response.Success(seller))
+	}
 }
+
 func (handler *SellerHandler) FindByUsername(ctx *gin.Context) {
 	username := ctx.Query("username")
 	if username != "" {
