4d6963792d000db45d30db8663242378edf8fc29
Dzkzzz <dzdzaakk@gmail.com>
2025-04-13 00:00:55 +0700

    generate new wire for auth

--- internal/Auth/injector/wire.go
+++ internal/Auth/injector/wire.go
@@ -2,3 +2,34 @@
 // +build wireinject
 
 package injector
+
+import (
+	"Dzaakk/simple-commerce/internal/auth/handlers"
+	"Dzaakk/simple-commerce/internal/auth/repositories"
+	"Dzaakk/simple-commerce/internal/auth/routes"
+	"Dzaakk/simple-commerce/internal/auth/usecases"
+	customerRepo "Dzaakk/simple-commerce/internal/customer/repositories"
+	customerUsecase "Dzaakk/simple-commerce/internal/customer/usecases"
+	sellerRepo "Dzaakk/simple-commerce/internal/seller/repositories"
+	sellerUsecase "Dzaakk/simple-commerce/internal/seller/usecases"
+	shoppingCartRepo "Dzaakk/simple-commerce/internal/shopping_cart/repositories"
+	"database/sql"
+
+	"github.com/google/wire"
+)
+
+func InitializedService(db *sql.DB) *routes.AuthRoutes {
+	wire.Build(
+		repositories.NewAuthRepository,
+		sellerRepo.NewSellerRepository,
+		customerRepo.NewCustomerRepository,
+		shoppingCartRepo.NewShoppingCartRepository,
+		usecases.NewAuthUseCase,
+		customerUsecase.NewCustomerUseCase,
+		sellerUsecase.NewSellerUseCase,
+		handlers.NewAtuhHandler,
+		routes.NewAuthRoutes,
+	)
+
+	return &routes.AuthRoutes{}
+}
