1d2a7d5a7d505c7700e1020b65d6ffed516cf154
Dzkzzz <dzdzaakk@gmail.com>
2025-04-13 00:12:35 +0700

    create seller route and generate seller wire

--- internal/seller/injector/wire.go
+++ internal/seller/injector/wire.go
@@ -1,1 +1,26 @@
+//go:build wireinject
+// +build wireinject
+
 package injector
+
+import (
+	"Dzaakk/simple-commerce/internal/seller/handlers"
+	"Dzaakk/simple-commerce/internal/seller/repositories"
+	"Dzaakk/simple-commerce/internal/seller/routes"
+	"Dzaakk/simple-commerce/internal/seller/usecases"
+
+	"database/sql"
+
+	"github.com/google/wire"
+)
+
+func InitializedService(db *sql.DB) *routes.SellerRoutes {
+	wire.Build(
+		repositories.NewSellerRepository,
+		usecases.NewSellerUseCase,
+		handlers.NewSellerHandler,
+		routes.NewSellerRoutes,
+	)
+
+	return &routes.SellerRoutes{}
+}
--- internal/seller/routes/seller_route.go
+++ internal/seller/routes/seller_route.go
@@ -1,1 +1,27 @@
 package routes
+
+import (
+	handler "Dzaakk/simple-commerce/internal/seller/handlers"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v8"
+)
+
+type SellerRoutes struct {
+	Handler *handler.SellerHandler
+}
+
+func NewSellerRoutes(handler *handler.SellerHandler) *SellerRoutes {
+	return &SellerRoutes{Handler: handler}
+}
+
+func (sr *SellerRoutes) Route(r *gin.RouterGroup, redis *redis.Client) {
+	sellerHandler := r.Group("/api/v1/sellers")
+
+	sellerHandler.Use()
+	{
+		sellerHandler.GET("", sr.Handler.FindAll)
+		sellerHandler.GET("/id", sr.Handler.FindById)
+		sellerHandler.GET("/username", sr.Handler.FindByUsername)
+	}
+}
