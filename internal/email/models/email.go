43976659bb2cd45d8043a6b661c6376edbedae69
Dzkzzz <dzdzaakk@gmail.com>
2025-04-15 23:48:06 +0700

    initialize model for email service

--- internal/email/models/email.go
+++ internal/email/models/email.go
@@ -1,1 +1,17 @@
 package models
+
+type EmailRequest struct {
+	Sender      Sender      `json:"sender"`
+	To          []Recipient `json:"to"`
+	Subject     string      `json:"subject"`
+	HTMLContent string      `json:"htmlContent"`
+}
+
+type Sender struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+type Recipient struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
