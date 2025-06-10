b32b2409c489913c9c6bfefb2aa68c6625d366ce
Dzkzzz <dzdzaakk@gmail.com>
2025-06-11 00:25:27 +0700

    remove auth repository and helper

--- internal/auth/repository/auth_repository.go
+++ internal/auth/repository/auth_repository.go
@@ -1,16 +1,8 @@
 package repository
 
 import (
-	"Dzaakk/simple-commerce/internal/auth/model"
 	"context"
 )
-
-type AuthRepository interface {
-	InsertCustomerCodeActivation(c context.Context, data model.TCustomerActivationCode) error
-	FindCodeByCustomerID(c context.Context, customerID int64) (*model.TCustomerActivationCode, error)
-	InsertSellerCodeActivation(c context.Context, data model.TSellerActivationCode) error
-	FindCodeBySellerID(c context.Context, sellerID int64) (*model.TSellerActivationCode, error)
-}
 
 type AuthCacheRepository interface {
 	SetActivationCustomer(c context.Context, email string, activationCode string) error
