0983f5076961e41a96dd8a3cd18600a9488c4f86
Dzkzzz <dzdzaakk@gmail.com>
2025-06-16 23:35:23 +0700

    fix login seller flow, remove redis param activation seller, implement activation seller, adjust req activation data customer

--- internal/auth/handler/auth_handler.go
+++ internal/auth/handler/auth_handler.go
@@ -5,11 +5,9 @@ import (
 	"Dzaakk/simple-commerce/internal/auth/usecase"
 	sellerUsecase "Dzaakk/simple-commerce/internal/seller/usecase"
 	"Dzaakk/simple-commerce/package/response"
-	"Dzaakk/simple-commerce/package/util"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/go-redis/redis/v8"
 )
 
 type AuthHandler struct {
@@ -42,7 +40,7 @@ func (h *AuthHandler) RegistrationCustomer(ctx *gin.Context) {
 }
 
 func (h *AuthHandler) ActivationCustomer(ctx *gin.Context) {
-	var data model.CustomerActivationReq
+	var data model.ActivationReq
 
 	if err := ctx.ShouldBindJSON(&data); err != nil {
 		ctx.JSON(http.StatusBadRequest, response.InvalidRequestData())
@@ -82,15 +80,32 @@ func (h *AuthHandler) RegistrationSeller(ctx *gin.Context) {
 		ctx.JSON(http.StatusInternalServerError, response.InternalServerError(err.Error()))
 		return
 	}
-	_, err := h.Usecase.SellerRegistration(ctx, data)
+
+	err := h.Usecase.RegistrationSeller(ctx, data)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, response.InternalServerError(err.Error()))
 		return
 	}
+
 	ctx.JSON(http.StatusOK, response.Success("Success Create Seller"))
 }
-func (h *AuthHandler) ActivationSeller(ctx *gin.Context, r *redis.Client) {
 
+func (h *AuthHandler) ActivationSeller(ctx *gin.Context) {
+
+	var data model.ActivationReq
+
+	if err := ctx.ShouldBindJSON(&data); err != nil {
+		ctx.JSON(http.StatusBadRequest, response.InvalidRequestData())
+		return
+	}
+
+	err := h.Usecase.ActivationSeller(ctx, data)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, response.InternalServerError(err.Error()))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, response.Success("Success Activate User"))
 }
 
 func (h *AuthHandler) LoginSeller(ctx *gin.Context) {
@@ -101,22 +116,11 @@ func (h *AuthHandler) LoginSeller(ctx *gin.Context) {
 		return
 	}
 
-	data, err := h.SellerUsecase.FindByEmail(ctx, reqData.Email)
+	err := h.Usecase.LoginSeller(ctx, reqData)
 	if err != nil {
 		ctx.JSON(http.StatusBadRequest, response.InvalidEmailOrPassword())
 		return
 	}
-
-	if !util.CheckPasswordHash(reqData.Password, data.Password) {
-		ctx.JSON(http.StatusBadRequest, response.InvalidEmailOrPassword())
-		return
-	}
-	// byteData, err := json.Marshal(data)
-	// if err != nil {
-	// 	ctx.JSON(http.StatusInternalServerError, response.InternalServerError(err.Error()))
-	// 	return
-	// }
-	// _ = auth.NewTokenGenerator(r, byteData)
 
 	ctx.JSON(http.StatusOK, response.Success("Login Success"))
 }
