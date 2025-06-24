62b6d4d767f56ef885b3a1b33701d7a594e2cb62
Dzkzzz <dzdzaakk@gmail.com>
2025-06-24 21:56:19 +0700

    fix(transaction): remove timeout and unecessary validation

--- internal/transaction/repository/transaction_repository_impl.go
+++ internal/transaction/repository/transaction_repository_impl.go
@@ -5,7 +5,6 @@ import (
 	response "Dzaakk/simple-commerce/package/response"
 	"context"
 	"database/sql"
-	"time"
 )
 
 type TransactionRepositoryImpl struct {
@@ -20,18 +19,11 @@ func NewTransactionRepository(db *sql.DB) TransactionRepository {
 
 const (
 	queryCreateTransaction = `INSERT INTO public.transaction (customer_id, cart_id, total_amount, transaction_date, status, created, created_by) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id`
-	dbQueryTimeout         = 3 * time.Second
 )
 
-func (repo *TransactionRepositoryImpl) contextWithTimeout(ctx context.Context) (context.Context, context.CancelFunc) {
-	return context.WithTimeout(ctx, dbQueryTimeout)
-}
-
 func (repo *TransactionRepositoryImpl) Create(ctx context.Context, data model.TTransaction) (*model.TTransaction, error) {
-	c, cancel := repo.contextWithTimeout(ctx)
-	defer cancel()
 
-	result, err := repo.DB.ExecContext(c, queryCreateTransaction, data.CustomerID, data.CartID, data.TotalAmount, data.TransactionDate, data.Status, data.Base.Created, data.Base.CreatedBy)
+	result, err := repo.DB.ExecContext(ctx, queryCreateTransaction, data.CustomerID, data.CartID, data.TotalAmount, data.TransactionDate, data.Status, data.Base.Created, data.Base.CreatedBy)
 	if err != nil {
 		return nil, response.ExecError("create transaction", err)
 	}
@@ -42,8 +34,6 @@ func (repo *TransactionRepositoryImpl) Create(ctx context.Context, data model.TT
 	return &data, nil
 }
 func (repo *TransactionRepositoryImpl) CreateWithTx(ctx context.Context, tx *sql.Tx, data model.TTransaction) (*model.TTransaction, error) {
-	c, cancel := repo.contextWithTimeout(ctx)
-	defer cancel()
 
 	statement, err := tx.Prepare(queryCreateTransaction)
 	if err != nil {
@@ -53,7 +43,7 @@ func (repo *TransactionRepositoryImpl) CreateWithTx(ctx context.Context, tx *sql
 
 	var id int
 
-	err = statement.QueryRowContext(c, data.CustomerID, data.CartID, data.TotalAmount, data.TransactionDate, data.Status, data.Base.Created, data.Base.CreatedBy).Scan(&id)
+	err = statement.QueryRowContext(ctx, data.CustomerID, data.CartID, data.TotalAmount, data.TransactionDate, data.Status, data.Base.Created, data.Base.CreatedBy).Scan(&id)
 	if err != nil {
 		return nil, err
 	}
