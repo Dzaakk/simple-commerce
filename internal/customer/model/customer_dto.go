290583ea5ead563d45116cbc86dd2dcc21706bb6
Dzkzzz <dzdzaakk@gmail.com>
2025-07-21 22:37:44 +0700

    fix(customer): add date of birth and address for update request, create method for converting update request data

--- internal/customer/model/customer_dto.go
+++ internal/customer/model/customer_dto.go
@@ -1,11 +1,34 @@
 package model
 
+import (
+	"Dzaakk/simple-commerce/package/template"
+	"database/sql"
+	"time"
+)
+
 type UpdateReq struct {
 	CustomerID  string `json:"customer_id" validate:"required,min=1"`
 	Username    string `json:"username" validate:"required"`
 	Email       string `json:"email" validate:"required,email"`
 	PhoneNumber string `json:"phone_number" validate:"required"`
+	DateOfBirth string `json:"date_of_birth" validate:"required,datetime=02-01-2006"`
+	Address     string `json:"address"`
 }
+
+func (req UpdateReq) ToCustomerModel(dateOfBirth time.Time, customerID int64) TCustomers {
+	return TCustomers{
+		ID:          customerID,
+		Username:    req.Username,
+		Email:       req.Email,
+		PhoneNumber: req.PhoneNumber,
+		DateOfBirth: sql.NullTime{Time: dateOfBirth, Valid: true},
+		Address:     req.Address,
+		Base: template.Base{
+			UpdatedBy: sql.NullString{String: req.Username, Valid: true},
+		},
+	}
+}
+
 type ChangePasswordReq struct {
 	NewPassword string `json:"newPassword" validate:"required"`
 }
