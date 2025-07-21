40cda91474fe6ae74bf4a300c6ac3c889d26e084
Dzkzzz <dzdzaakk@gmail.com>
2025-07-21 22:34:38 +0700

    fix(auth): change id customer to int64

--- internal/auth/model/auth.go
+++ internal/auth/model/auth.go
@@ -3,7 +3,7 @@ package model
 import "github.com/golang-jwt/jwt/v5"
 
 type CustomerToken struct {
-	ID       int    `json:"id"`
+	ID       int64  `json:"id"`
 	Username string `json:"username"`
 	Email    string `json:"email"`
 	Role     string `json:"role"`
