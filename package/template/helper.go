ebc6f41516a6425e9df00944eaa833d8b11dbffe
Dzkzzz <dzdzaakk@gmail.com>
2026-02-04 23:44:23 +0700

    change return value to bool

--- package/template/helper.go
+++ package/template/helper.go
@@ -1,17 +1,13 @@
 package template
 
 import (
-	"Dzaakk/simple-commerce/package/response"
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
-func AuthorizedChecker(ctx *gin.Context, customerId string) {
-	currentId, exists := ctx.Get("customerId")
-	if !exists || currentId != customerId {
-		ctx.JSON(http.StatusUnauthorized, response.Unauthorized(nil))
-		ctx.Abort()
-		return
+func AuthorizedChecker(ctx *gin.Context, customerID string) bool {
+	currentID, exists := ctx.Get("customerId")
+	if !exists || currentID != customerID {
+		return false
 	}
+	return true
 }
