d975c3e89a99ebd58de1000a7305ba4bff370d54
Dzkzzz <dzdzaakk@gmail.com>
2026-02-08 00:40:55 +0700

    update(auth-route): create init func from route

--- internal/auth/route/auth_route.go
+++ internal/auth/route/auth_route.go
@@ -2,9 +2,18 @@ package route
 
 import (
 	"Dzaakk/simple-commerce/internal/auth/handler"
+	"Dzaakk/simple-commerce/internal/auth/repository"
+	"Dzaakk/simple-commerce/internal/auth/usecase"
+	customerRepo "Dzaakk/simple-commerce/internal/customer/repository"
+	customerUsecase "Dzaakk/simple-commerce/internal/customer/usecase"
+	emailUsecase "Dzaakk/simple-commerce/internal/email/usecase"
 	middleware "Dzaakk/simple-commerce/internal/middleware/jwt"
+	sellerRepo "Dzaakk/simple-commerce/internal/seller/repository"
+	shoppingCartRepo "Dzaakk/simple-commerce/internal/shopping_cart/repository"
+	"database/sql"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v8"
 )
 
 type AuthRoutes struct {
@@ -39,4 +48,27 @@ func (ar *AuthRoutes) Route(r *gin.RouterGroup) {
 		seller.POST("/login", ar.Handler.LoginSeller)
 		seller.POST("/logout", ar.SellerMiddleware.ValidateToken(), ar.Handler.Logout)
 	}
+}
+
+func InitializedService(db *sql.DB, redis *redis.Client) *AuthRoutes {
+	customerRepository := customerRepo.NewCustomerRepository(db)
+	customerUsecase := customerUsecase.NewCustomerUseCase(customerRepository)
+
+	sellerRepository := sellerRepo.NewSellerRepository(db)
+	shoppingCartRepository := shoppingCartRepo.NewShoppingCartRepository(db)
+
+	authCacheCustomer := repository.NewAuthCacheCustomerRepository(redis)
+	authCacheSeller := repository.NewAuthCacheSellerRepository(redis)
+
+	authUsecase := usecase.NewAuthUsecase(authCacheCustomer, authCacheSeller, customerUsecase, sellerRepository, shoppingCartRepository)
+	mailer := emailUsecase.NewEmailUseCase()
+	handler := handler.NewAtuhHandler(authUsecase, mailer)
+
+	authCustomerToken := usecase.NewAuthCustomerTokenUsecase(authCacheCustomer)
+	authSellerToken := usecase.NewAuthSellerTokenUsecase(authCacheSeller)
+
+	customerMiddleware := middleware.NewJWTCustomerMiddleware(authCustomerToken)
+	sellerMiddleware := middleware.NewJWTSellerMiddleware(authSellerToken)
+
+	return NewAuthRoutes(handler, customerMiddleware, sellerMiddleware)
 }
