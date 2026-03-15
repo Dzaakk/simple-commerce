69c89bb3fa6da929b2d735c8da0832528d720d9c
Dzkzzz <dzdzaakk@gmail.com>
2026-03-16 06:22:24 +0700

    add response error user not found

--- package/response/response_error.go
+++ package/response/response_error.go
@@ -9,7 +9,7 @@ var (
 	// ErrAccountNotActive   = errors.New("account is not active, please verify your email")
 
 	// Activation
-	// ErrInvalidCode     = errors.New("activation code is invalid")
+	ErrInvalidActivationCode = errors.New("activation code is invalid")
 	// ErrExpiredCode     = errors.New("activation code has expired")
 	// ErrCodeAlreadyUsed = errors.New("activation code has already been used")
 
@@ -17,4 +17,12 @@ var (
 	// ErrInvalidToken = errors.New("token is invalid")
 	// ErrExpiredToken = errors.New("token has expired")
 	// ErrRevokedToken = errors.New("token has been revoked")
+)
+
+var (
+	// ErrInternalServer = errors.New("internal server error")
+	// ErrBadRequest     = errors.New("bad request")
+	// ErrUnauthorized   = errors.New("unauthorized")
+	// ErrForbidden      = errors.New("forbidden")
+	ErrUserNotFound = errors.New("user not found")
 )
